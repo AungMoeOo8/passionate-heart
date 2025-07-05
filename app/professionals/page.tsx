93cea910bef3779d30ce82d1faa02b24d7106fb2
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2025-07-05 07:08:12 +0700

    feat: make get people request

--- app/professionals/page.tsx
+++ app/professionals/page.tsx
@@ -1,6 +1,26 @@
 import { Metadata } from "next";
 import { ProfessionalCard } from "../../components";
-import counsellorsData from "../../staticData/professionals";
+
+const { NEXT_WORDPRESS_DOMAIN } = process.env;
+
+export const dynamic = 'force-dynamic';
+
+export type Response<TData> = {
+  isSuccess: boolean;
+  message: string;
+  data: TData;
+};
+
+export type PersonProps = {
+  id: string;
+  name: string;
+  image: string;
+  position: string;
+  roles: string[];
+  biography: string;
+  visibility: boolean;
+  indexNumber: number;
+};
 
 export const metadata: Metadata = {
   title: "Mental Health Professionals & Practitioners",
@@ -11,7 +31,11 @@ export const metadata: Metadata = {
   },
 };
 
-export default function Page() {
+export default async function Page() {
+  const res = await fetch(`${NEXT_WORDPRESS_DOMAIN}/phweb/wp-json/api/people`);
+  const { data: counsellors }: Response<PersonProps[]> = await res.json();
+  console.log(counsellors);
+
   return (
     <>
       <div className="mt-[50px]">
@@ -21,12 +45,12 @@ export default function Page() {
       </div>
       <div className="mb-[100px]">
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-[20px]">
-          {counsellorsData.map((counsellor, index) => (
+          {counsellors.sort((a, b) => a.indexNumber - b.indexNumber).map((counsellor, index) => (
             <li key={index} className="flex justify-center rounded-[10px]">
               <ProfessionalCard
                 image={counsellor.image}
                 name={counsellor.name}
-                bio={counsellor.bio}
+                bio={counsellor.biography}
                 roles={counsellor.roles}
               />
             </li>
